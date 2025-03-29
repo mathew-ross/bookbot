b0b3746d2bbcfd0e0de0b0c506da255ff2331011
Mathew Ross <matjross@gmail.com>
2025-03-29 17:56:49 -0600

    added stats.py, edited main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+from stats import get_word_count
+
+def get_book_text(filepath):
+  with open(filepath) as f:
+    file_contents = f.read()
+  return file_contents
+
+
+def main():
+  file = "books/frankenstein.txt"
+  text = get_book_text(file)
+  # print(text)
+  print(f"{get_word_count(text)} words found in the document")
+
+
+main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_word_count(book_text):
+  word_list = book_text.split()
+  return len(word_list)
